2764d97a869909b068ce83e8b19dac6efeb3a4ed
JereUR <jeremias.jdv@gmail.com>
2023-07-14 02:42:52 -0300

    feat(Root): Navbar created

---
+++ components/Header.js
@@ -0,0 +1,46 @@
+import { Text, Navbar, useTheme, Container } from '@nextui-org/react'
+import { Layout } from './Layout'
+import { useRouter } from 'next/router'
+
+export function Header() {
+  const { isDark } = useTheme()
+  const router = useRouter()
+
+  return (
+    <Layout>
+      <Navbar isBordered={isDark} variant="sticky" shouldHideOnScroll>
+        <Navbar.Brand>
+          <Container display="inline-flex">
+            <Text component="span" color="inherit" weight={'bold'} size={30}>
+              New
+            </Text>
+            <Text component="span" color="inherit" size={32} weight={'light'}>
+              xkcd
+            </Text>
+          </Container>
+        </Navbar.Brand>
+        <Navbar.Content
+          activeColor="neutral"
+          hideIn="xs"
+          variant="highlight-solid"
+        >
+          <Navbar.Link isActive={router.pathname === '/'} href="/">
+            <Text size={18} weight={'medium'}>
+              Home
+            </Text>
+          </Navbar.Link>
+          <Navbar.Link isActive={router.pathname === '/about'} href="/about">
+            <Text size={18} weight={'medium'}>
+              About
+            </Text>
+          </Navbar.Link>
+          <Navbar.Link isActive={router.pathname === '/search'} href="/search">
+            <Text size={18} weight={'medium'}>
+              Search
+            </Text>
+          </Navbar.Link>
+        </Navbar.Content>
+      </Navbar>
+    </Layout>
+  )
+}
