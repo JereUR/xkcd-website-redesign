64a5e248a9e5af4b30adcb9f7cd6a8e832732175
JereUR <jeremias.jdv@gmail.com>
2023-07-14 02:43:36 -0300

    feat(Root): Page about created

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Header } from '../components/Header'
+import { Container } from '@nextui-org/react'
+
+export default function about() {
+  return (
+    <Container>
+      <Header />
+      <div>about</div>
+    </Container>
+  )
+}
