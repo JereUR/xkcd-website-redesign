be506f8c8f3be44ee72860022dfbd489153ee243
JereUR <jeremias.jdv@gmail.com>
2023-07-25 21:37:33 -0300

    Feat(Root): In "icon website" redirect to home

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,9 @@ export function Header() {
   return (
     <header className="flex justify-between items-center p-4 max-w-xl m-auto">
       <h1 className="font-bold text-3xl">
-        new<span className="font-light text-2xl">xkcd</span>
+        <Link href="/" className="text-black transition hover:opacity-80">
+          new<span className="font-light text-2xl">xkcd</span>
+        </Link>
       </h1>
       <nav>
         <ul className="flex flex-row gap-2">
