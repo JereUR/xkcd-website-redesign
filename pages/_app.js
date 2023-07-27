e541735d9d34be985be2e44b89dcef62c37c253f
JereUR <jeremias.jdv@gmail.com>
2023-07-26 22:10:57 -0300

    feat(Root): link to general favicon added

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import { NextUIProvider } from '@nextui-org/react'
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <NextUIProvider>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </NextUIProvider>
   )
