cb14119817a1f350f757b211cc8fc61b4b2bad75
JereUR <jeremias.jdv@gmail.com>
2023-07-27 21:50:30 -0300

    feat(Root). Search microservice

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,3 +1,11 @@
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import { search } from 'services/search'
+
+export default async function handler(req, res) {
+  const {
+    query: { q }
+  } = req
+
+  const { results } = await search({ query: q })
+
+  return res.status(200).json(results)
 }
