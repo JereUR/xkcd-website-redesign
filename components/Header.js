df08cd5acf53f6d800749262915b27ce36eb15b5
JereUR <jeremias.jdv@gmail.com>
2023-07-27 21:49:40 -0300

    feat(Root): Input for comic searchs

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,24 @@
 import { Link } from '@nextui-org/react'
+import { useRef, useState } from 'react'
 
 export function Header() {
+  const [results, setResults] = useState([])
+  const searchRef = useRef()
+
+  const q = searchRef.current?.value
+
+  const handleChange = () => {
+    if (q.trim() === '') {
+      setResults([])
+    } else {
+      fetch(`/api/search?q=${q}`)
+        .then((res) => res.json())
+        .then((results) => {
+          setResults(results)
+        })
+    }
+  }
+
   return (
     <header className="flex justify-between items-center p-4 max-w-xl m-auto">
       <h1 className="font-bold text-3xl">
@@ -16,9 +34,40 @@ export function Header() {
             </Link>
           </li>
           <li>
-            <Link href="/search" className="text-sm font-bold text-black">
-              Search
-            </Link>
+            <input
+              className="text-sm rounded-3xl border-gray-400 border-2 px-4 py-1"
+              ref={searchRef}
+              type="search"
+              onChange={handleChange}
+            />
+            <div className="relative z-10">
+              {Boolean(results.length) && (
+                <div className="absolute top-0 left-0">
+                  <ul className="w-full border border-gray-50 rounded-lg shadow-xl bg-white overflow-hidden">
+                    <li className="m-0" key="all-results">
+                      <Link
+                        href={`/search?q=${q}`}
+                        className="italic px-2 py-1 text-sm font-semibold text-black hover:bg-slate-200 w-full text-ellipsis whitespace-nowrap text-gray-400"
+                      >
+                        Ver {results.length} resultados
+                      </Link>
+                    </li>
+                    {results.map((result) => {
+                      return (
+                        <li className="m-0" key={result.id}>
+                          <Link
+                            href={`/comic/${result.id}`}
+                            className="px-2 py-1 text-sm font-semibold text-black hover:bg-slate-200 w-full text-ellipsis whitespace-nowrap"
+                          >
+                            {result.title}
+                          </Link>
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </div>
+              )}
+            </div>
           </li>
         </ul>
       </nav>
