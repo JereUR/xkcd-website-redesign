47cacd4cbd78e4994112fd57093e74cbbab81549
JereUR <jeremias.jdv@gmail.com>
2023-07-28 22:34:12 -0300

    feat(Root): i18n used

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,11 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
 import Layout from 'components/Layout'
+import { useI18N } from 'context/i18n'
 
 export default function Home({ latestComics }) {
+  const { t } = useI18N()
+
   return (
     <>
       <Head>
@@ -14,13 +17,15 @@ export default function Home({ latestComics }) {
       </Head>
 
       <Layout>
-        <h2 className="text-3xl font-bold text-center mb-10">Latest Comics</h2>
+        <h2 className="text-3xl font-bold text-center mb-10">
+          {t('LATEST_COMICS')}
+        </h2>
         <section className="grid grid-cols-1 gap-2 max-w-md m-auto sm:grid-cols-2 md:grid-cols-3">
           {latestComics.map((comic) => {
             return (
               <Link href={`/comic/${comic.id}`} key={comic.id}>
                 <a className="mb-4 pb-4 m-auto">
-                  <h3 className="font-semibold text-sm text-center pb-2">
+                  <h3 className="font-bold text-sm text-center pb-2">
                     {comic.title}
                   </h3>
                   <Image
